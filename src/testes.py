73d21c389037fe7b976eaf582985287dac484e94
AndrewMarinho <54558772+AndrewMarinho@users.noreply.github.com>
2020-04-18 17:28:17 -0300

    Update testes.py

--- src/testes.py
+++ src/testes.py
@@ -8,7 +8,7 @@ if len(jogo) != 3:
     erroIniciar = True
 else:
     for linha in jogo:
-        if len(linha) != 3
+        if len(linha) != 3:
             erroInicializar = True
         else:
             for elemento em linha:
